bc7a75c839cd97cec2591901a49b39b37f6ec429
samuelcrb <samuastur14@gmail.com>
2022-04-20 11:41:43 +0200

    Creada clase calculadora con operaciones basicas

--- calculadora.js
+++ calculadora.js
@@ -1,6 +1,28 @@
-function suma(num1, num2) {}
-function resta(num1, num2) {}
-function multiplicacion(num1, num2) {}
-function division(num1, num2) {}
-
+'use strict'
 module.exports = { suma, resta, multiplicacion, division };
+
+class Calculadora{
+
+    constructor(num1,num2,operation,result){
+        this.num1 = num1;
+        this.num2 = num2;
+        this.operation = operation;
+        this.result = result;
+    }
+
+    suma(num1,num2){
+        return num1 + num2;
+    }
+
+    resta(num1,num2){
+        return num1 - num2;
+    }
+
+    producto(num1,num2){
+        return num1 * num2;
+    }
+
+    cociente(num1,num2){
+        return num1/num2;
+    }
+}
