1f008f76a7fcb3b02d98c3c59ea96563724f0cf6
samuelcrb <samuastur14@gmail.com>
2022-04-21 21:55:08 +0200

    terminado el main

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const outputValorAnterior = document.getElementById('primerValor');
+const outputValorActual = document.getElementById('segundoValor');
+const numeros = document.querySelectorAll('.boton_numero');
+const operadores = document.querySelectorAll('.boton_signo');
+
+const output = new Output(outputValorAnterior, outputValorActual);
+
+numeros.forEach(boton => {
+    boton.addEventListener('click', () => output.añadirNumero(boton.innerHTML));
+});
+
+operadores.forEach(boton => {
+    boton.addEventListener('click', () => output.operar(boton.value))
+});
