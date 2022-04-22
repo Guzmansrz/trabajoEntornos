189084066ba773a1afcbce64cd361b5c04cecfd1
Guzmansrz <guzmansuarezalvarez@gmail.com>
2022-04-22 12:42:56 +0200

    Merge branch 'main' of https://github.com/Guzmansrz/trabajoEntornos

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,19 +1,19 @@
-'use strict'
-class Calculadora{
+"use strict";
 
-    suma(num1,num2){
-        return num1 + num2;
-    }
+module.exports = class Calculadora {
+  suma(num1, num2) {
+    return num1 + num2;
+  }
 
-    resta(num1,num2){
-        return num1 - num2;
-    }
+  resta(num1, num2) {
+    return num1 - num2;
+  }
 
-    producto(num1,num2){
-        return num1 * num2;
-    }
+  producto(num1, num2) {
+    return num1 * num2;
+  }
 
-    cociente(num1,num2){
-         return num1/num2;
-    }
-}
+  cociente(num1, num2) {
+    return num1 / num2;
+  }
+};
