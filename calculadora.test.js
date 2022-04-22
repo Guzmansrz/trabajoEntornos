5dad3375f88f95450ad1aa8d026dd39571cfd48d
Guzmansrz <guzmansuarezalvarez@gmail.com>
2022-04-22 08:37:06 +0200

    incorporar cambios

--- calculadora.test.js
+++ calculadora.test.js
@@ -22,28 +22,28 @@ describe("funcion resta", () => {
     expect(calculadora.resta(12, 0)).toBe(12);
   });
 });
-describe("funcion multiplicacion", () => {
-  test("multiplicacion numeros positivos enteros", () => {
-    expect(calculadora.multiplicacion(2, 34)).toBe(68);
+describe("funcion producto", () => {
+  test("producto numeros positivos enteros", () => {
+    expect(calculadora.producto(2, 34)).toBe(68);
   });
-  test("multiplicacion numeros positivos decimales", () => {
-    expect(calculadora.multiplicacion(2.1, 3.4)).toBe(7.14);
+  test("producto numeros positivos decimales", () => {
+    expect(calculadora.producto(2.1, 3.4)).toBe(7.14);
   });
-  test("multiplicacion un numero es cero", () => {
-    expect(calculadora.multiplicacion(12, 0)).toBe(0);
+  test("producto un numero es cero", () => {
+    expect(calculadora.producto(12, 0)).toBe(0);
   });
 });
-describe("funcion division", () => {
-  test("division numeros positivos exacta", () => {
-    expect(calculadora.division(34, 2)).toBe(17);
+describe("funcion cociente", () => {
+  test("cociente numeros positivos exacta", () => {
+    expect(calculadora.cociente(34, 2)).toBe(17);
   });
-  test("division numeros positivos no exacta", () => {
-    expect(calculadora.division(14, 4)).toBe(3.5);
+  test("cociente numeros positivos no exacta", () => {
+    expect(calculadora.cociente(14, 4)).toBe(3.5);
   });
-  test("division un dividendo es cero", () => {
-    expect(calculadora.division(0, 2)).toBe(0);
+  test("cociente un dividendo es cero", () => {
+    expect(calculadora.cociente(0, 2)).toBe(0);
   });
-  test("division un divisor es cero", () => {
-    expect(calculadora.division(2, 0)).toBe("No se puede dividir por cero");
+  test("cociente un divisor es cero", () => {
+    expect(calculadora.cociente(2, 0)).toBe("No se puede dividir por cero");
   });
 });
